f654325e5ce089ca750edf5a4c5b39bf7ee04836
brandonsabino <brandonsabino1@gmail.com>
2023-10-20 20:04:06 -0400

    style: Update general markup and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
+import Navigation from './Navigation.js'
+import Main from './Main.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Totally under construction, sry
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+      {/* <Navigation/> */}
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <main>
+            <h1>Brandon Sabino</h1>
+            <p>Howdy! I'm a front end developer based out of NYC currently working for <a href="https://www.rightpoint.com/">Rightpoint</a></p>
+            <p>In my free time, the little that my corporate overlords allow, I enjoy playing guitar, exploring the city on my bike, and building npm packages.</p>
+            <p>Want to connect? Find me on...</p>
+            <ul>
+                <li><a href="https://github.com/brandonsabino">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/brandon-sabino-88ba44125">LinkedIn</a></li>
+            </ul>
+        </main>
+    )
+}
+
+export default Main;
