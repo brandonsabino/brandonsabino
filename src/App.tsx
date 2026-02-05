24909aadf28c72a72e2e7bfef0e12c0d362fe663
brandonsabino <brandonsabino1@gmail.com>
2026-02-04 21:25:49 -0500

    upgrade: Update app shell and public assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import './App.css';
 import WelcomeSection from './components/WelcomeSection';
-import HexMap from './components/HexMap';
-import './components/HexMap.css';
+import ContentSection from './components/ContentSection';
 
 function App(): React.JSX.Element {
   return (
     <div className="App">
       <WelcomeSection />
-      <section className="honeycomb-section">
-        <HexMap />
-      </section>
+      <ContentSection />
     </div>
   );
 }
